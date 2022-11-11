7719d6be1b093d4f643b3b4cf5466d3799c00eea
Nawfaal Rahaman <71569980+NawfaalRahaman@users.noreply.github.com>
2022-11-11 09:51:15 +0300

    Update movesDB.py

--- movesDB.py
+++ movesDB.py
@@ -62,11 +62,127 @@ fury swipes =
 glare =
 """
 growl = Move("Growl", 0, "Normal", 40)
-
-scratch = Move("Scratch", 40, "Normal", 35)
-tailWhip = Move("Tail Whip", 0, "Normal", 30)
-tackle = Move("Tackle", 35, "Normal", 35)
-waterGun = Move("Water Gun", 40, "Water", 25)
+"""
+growth =
+guillotine =
+gust =
+harden =
+haze =
+headbutt =
+hi jump kick =
+horn attack =
+horn drill =
+hydro pump =
+hyper beam =
+hyper fang =
+hypnosis =
+ice beam =
+ice punch =
+jump kick =
+karate chop =
+kinesis =
+leech life =
+"""
 leechSeed = Move("Leech Seed", 0, "Grass", 10)
+"""
+leer =
+lick =
+light screen =
+lovely kiss =
+low kick =
+meditate =
+mega drain =
+mega kick =
+mega punch =
+metronome =
+mimic =
+minimize =
+mirror move =
+mist =
+night shade =
+pay day =
+peck =
+petal dance =
+pin missile =
+poison gas =
+poison sting =
+poisonpowder =
+pound =
+psybeam =
+psychic =
+psywave =
+quick attack =
+rage =
+razor leaf =
+razor wind =
+recover =
+reflect =
+rest =
+roar =
+rock slide =
+rock throw =
+rolling kick =
+sand-attack =
+"""
+scratch = Move("Scratch", 40, "Normal", 35)
+"""
+screech =
+seismic toss =
+selfdestruct =
+sharpen =
+sing =
+skull bash =
+sky attack =
+slam =
+slash =
+sleep powder =
+sludge =
+smog =
+smokescreen =
+softboiled =
+solarbeam =
+sonicboom =
+spike cannon =
+splash =
+spore =
+stomp =
+strength =
+string shot =
+struggle =
+stun spore =
+submission =
+substitute =
+super fang =
+supersonic =
+surf =
+swift =
+swords dance =
+"""
+tackle = Move("Tackle", 35, "Normal", 35)
+tailWhip = Move("Tail Whip", 0, "Normal", 30)
+"""
+take down =
+teleport =
+thrash =
+thunder =
+thunder wave =
+thunderbolt =
+thunderpunch =
+thundershock =
+toxic =
+transform =
+tri attack =
+twineedle =
+vicegrip =
+vine whip =
+"""
+waterGun = Move("Water Gun", 40, "Water", 25)
+"""
+waterfall =
+whirlwind =
+wing attack =
+withdraw =
+wrap =
+"""
 
 #TODO The other pokemon moves in pokemonDB
