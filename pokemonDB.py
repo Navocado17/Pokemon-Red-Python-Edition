8fcd19ed71a46069b77fa37fd6e8b9d185910576
Navodith <navodithmanoj171106@gmail.com>
2022-11-11 10:41:56 +0300

    Update pokemonDB.py

--- pokemonDB.py
+++ pokemonDB.py
@@ -1,28 +1,31 @@
 import movesDB
 
 class Pokemon:
-  def __init__(self, species, type, name, level, gender, move1, move2, move3, hp, attack, defense): #TODO Add Special and Speed
+  def __init__(self, species, type, name, level, move1, move2, move3, move4, hp, attack, defense, special, speed): #TODO Add Special and Speed
     self.species = species
     self.type = type
     self.name = name
     self.level = level
-    self.gender = gender
     self.move1 = move1
     self.move2 = move2
     self.move3 = move3
+    self.move4 = move4
     self.hp = hp
     self.attack = attack
     self.defense = defense
 
+    self.special = special
+    self.speed = speed
+
 
     
-
-charmander = Pokemon("CHARMANDER", "Fire", "CHARMANDER", 5, "M", movesDB.scratch, movesDB.growl, movesDB.ember, 39, 52, 43)
-squirtle = Pokemon("SQUIRTLE", "Water", "SQUIRTLE", 5, "M", movesDB.tackle, movesDB.tailWhip, movesDB.waterGun, 44, 48, 65)
-bulbasaur = Pokemon("BULBASAUR", "Grass", "BULBASAUR", 5, "M", movesDB.tackle, movesDB.growl, movesDB.leechSeed, 45, 49, 49)
-rattata = Pokemon("RATTATA", "Normal", "RATTATA", 5, "M", movesDB.tackle, movesDB.tailWhip, "Quick Attack", 0, 0, 0)
-pidgey = Pokemon("PIDGEY", "Normal", "PIDGEY", 5, "M", "Gust", "Sand-Attack", "Quick Attack", 0, 0, 0)
-caterpie = Pokemon("CATERPIE", "Bug", "CATERPIE", 5, "M", movesDB.tackle, "String Shot", "", 0, 0, 0)
-weedle = Pokemon("WEEDLE", "Bug", "WEEDLE", 5, "M", "Poison Sting", "String Shot", "", 0, 0, 0)
-nidoranM = Pokemon("NIDORAN", "Poison", "NIDORAN", 5, "M", "Leer", movesDB.tackle, "Horn Attack", 0, 0, 0)
-nidoranF = Pokemon("NIDORAN", "Poison", "NIDORAN", 5, "F", movesDB.growl, movesDB.tackle, movesDB.scratch, 0, 0, 0)
+charmander = Pokemon("CHARMANDER", "Fire", "CHARMANDER", 5, movesDB.scratch, movesDB.growl, movesDB.ember, "Smokescreen", 19, 11, 10, 11, 12)
+squirtle = Pokemon("SQUIRTLE", "Water", "SQUIRTLE", 5, movesDB.tackle, movesDB.tailWhip, movesDB.waterGun, "bubble", 20, 10, 12, 11, 10)
+bulbasaur = Pokemon("BULBASAUR", "Grass", "BULBASAUR", 5, movesDB.tackle, movesDB.growl, movesDB.leechSeed, 20, 10, 10, 13, 10)
+rattata = Pokemon("RATTATA", "Normal", "RATTATA", 5, movesDB.tackle, movesDB.tailWhip, "Quick Attack", "bite", 0, 0, 0, 0, 0)
+pidgey = Pokemon("PIDGEY", "Normal", "PIDGEY", 5, "Gust", "Sand-Attack", "Quick Attack", "Agility", 0, 0, 0, 0, 0)
+caterpie = Pokemon("CATERPIE", "Bug", "CATERPIE", 5, movesDB.tackle, "String Shot", "", "", 0, 0, 0, 0, 0)
+weedle = Pokemon("WEEDLE", "Bug", "WEEDLE", 5, "Poison Sting", "String Shot", "", "", 0, 0, 0, 0, 0)
+pikachu = Pokemon("PIKACHU", "Elecric", "PIKACHU", 5, "Thundershock", "growl", "", "", 0, 0, 0, 0, 0, 0)
+nidoranM = Pokemon("NIDORAN", "Poison", "NIDORAN", 5, "Leer", movesDB.tackle, "Horn Attack", "", 0, 0, 0, 0, 0)
+nidoranF = Pokemon("NIDORAN", "Poison", "NIDORAN", 5, movesDB.growl, movesDB.tackle, movesDB.scratch, "", 0, 0, 0, 0, 0)
